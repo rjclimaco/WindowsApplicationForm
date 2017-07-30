87ce423cb4fad4ed699f002a5a5ce3fe078fa300
rjclimaco <rjclimaco84@gmail.com>
2017-07-31 00:43:36 +0800

    first commit

---
+++ Midterm2/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Midterm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Lastname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Firstname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Password = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 217);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LastName:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Firstname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "UserName:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password:";
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(122, 296);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(246, 22);
+            this.txtlastname.TabIndex = 5;
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Location = new System.Drawing.Point(122, 342);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(246, 22);
+            this.txtfirstname.TabIndex = 6;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(122, 382);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(246, 22);
+            this.txtusername.TabIndex = 7;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(122, 433);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(246, 22);
+            this.txtpassword.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Lastname,
+            this.Firstname,
+            this.Username,
+            this.Password,
+            this.columnHeader1});
+            this.listView1.Location = new System.Drawing.Point(468, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(717, 416);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Lastname
+            // 
+            this.Lastname.Text = "Lastname";
+            this.Lastname.Width = 166;
+            // 
+            // Firstname
+            // 
+            this.Firstname.Text = "Firstname";
+            this.Firstname.Width = 135;
+            // 
+            // Username
+            // 
+            this.Username.Text = "Username";
+            this.Username.Width = 194;
+            // 
+            // Password
+            // 
+            this.Password.Text = "Password";
+            this.Password.Width = 371;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(988, 476);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Add";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(988, 514);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 21);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Update";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(988, 555);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(59, 21);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "View";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(988, 597);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(71, 21);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "Delete";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 46);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(614, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 46);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(723, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 46);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(836, 514);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(91, 46);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "VIEW";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 664);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtfirstname);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Lastname;
+        private System.Windows.Forms.ColumnHeader Firstname;
+        private System.Windows.Forms.ColumnHeader Username;
+        private System.Windows.Forms.ColumnHeader Password;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Midterm2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Midterm2
+{
+    public partial class Form1 : Form
+    {
+        string myCon = "server=localhost;database=database_login;Uid=root;Password=;";
+        string userid = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            View();
+        }
+        public void View()
+        {
+            listView1.Items.Clear();
+            MySqlConnection con = new MySqlConnection(myCon);
+            con.Open();
+            MySqlCommand retrieve = con.CreateCommand();
+            retrieve.Connection = con;
+            retrieve.CommandText = "Select * from tbl_users";
+            MySqlDataReader reader = retrieve.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListViewItem list = new ListViewItem(reader["Lastname"].ToString());
+                list.SubItems.Add(reader["Firstname"].ToString());
+                list.SubItems.Add(reader["Username"].ToString());
+                list.SubItems.Add(reader["Password"].ToString());
+                list.SubItems.Add(reader["id"].ToString());
+                listView1.Items.Add(list);
+            }
+            con.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+        public void Add()
+        {
+            MySqlConnection con = new MySqlConnection(myCon);
+            con.Open ();
+            MySqlCommand insert = con.CreateCommand();
+            insert.Connection = con;
+            insert.CommandText = "Insert into tbl_users(Lastname, Firstname, Username, Password) values ('" + txtlastname.Text +"' , '" + txtfirstname.Text + "', '" + txtusername.Text + "' , '" + txtpassword.Text + "')";
+            insert.ExecuteNonQuery();
+            con.Close();
+   
+            MessageBox.Show("Success");
+            View();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void Update()
+        {
+            MySqlConnection con = new MySqlConnection(myCon);
+            con.Open();
+            MySqlCommand insert = con.CreateCommand();
+            insert.Connection = con;
+            insert.CommandText = "UPDATE `tbl_users` SET `id`=[value - 1],`Lastname`=[value - 2],`Firstname`=[value - 3],`Username`=[value - 4],`Password`=[value - 5] WHERE 1";
+            insert.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count > 0)
+            {
+               
+            }
+        }
+    }
+}
